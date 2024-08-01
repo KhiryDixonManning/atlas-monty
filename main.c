f3607cbddf5df0f396499221d5989c3818351117
root <ManningKhiry@gmail.com>
2024-08-01 02:07:58 +0000

    please god

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "monty.h"
 
 FILE *file = NULL;
-
 /**
  * main - Hi Mr. Monty how are you?
  * @argc: The ticker for the arguments who come in
@@ -10,63 +9,107 @@ FILE *file = NULL;
  */
 int main(int argc, char **argv)
 {
-    void (*f)(stack_t **, unsigned int) = NULL;
-    char *buffer = NULL, op[50] = {'\0'}, pushNum[50] = {'\0'}, *token = NULL;
-    size_t bufsize = 0;
-    stack_t *stack = NULL;
-    unsigned int line_number = 1;
+	void (*f)(stack_t **, unsigned int) = NULL;
+	char *buffer = NULL, op[50] = {'\0'}, pushNum[50] = {'\0'}, *token = NULL;
+	size_t bufsize = 0;
+	stack_t *stack = NULL;
+	unsigned int line_number = 1;
 
-    if (argc != 2)
-        usage_error();
-
-    file = fopen(argv[1], "r");
-    if (!file)
-        file_open_error(argv[1]);
-
-    while (getline(&buffer, &bufsize, file) != -1)
-    {
-        token = strtok(buffer, " \t\n");
-        if (!token)
-        {
-            line_number++;
-            continue;
-        }
-
-        strcpy(op, token);
-        f = get_func(&stack, line_number, op);
-        if (!f)
-        {
-            free(buffer);
-            free_stack(stack);
-            fclose(file);
-            malloc_error();
-        }
-
-        if (strcmp(op, "push") == 0)
-        {
-            token = strtok(NULL, " \t\n");
-            if (!token || !is_integer(token))
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                free(buffer);
-                free_stack(stack);
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
-            int value = atoi(token);
-            stack = push(stack, value);
-        }
-        else
-        {
-            f(&stack, line_number);
-        }
-
-        line_number++;
-    }
-
-    free(buffer);
-    free_stack(stack);
-    fclose(file);
-    return 0;
+	if (argc != 2)
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	file = fopen(argv[1], "r");
+	if (!file)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	for (; getline(&buffer, &bufsize, file) != EOF; line_number++)
+	{
+		token = strtok((buffer), " \t\n");
+		if (!token)
+		{
+			free(buffer), buffer = NULL;
+			continue;
+		}
+		strcpy(op, token);
+		f = get_func(&stack, line_number, op);
+		if (!f)
+			fprintf(stderr, "Error: malloc failed\n"), error();
+		if (strcmp(op, "push") == 0)
+		{
+			token = strtok(NULL, " \t\n");
+			if (!token)
+			{
+				free(buffer), buffer = NULL, free_stack(&stack);
+				fprintf(stderr, "L%d: usage: push integer\n", line_number), error();
+			}
+			strcpy(pushNum, token);
+		}
+		free(buffer), buffer = NULL;
+		f(&stack, line_number);
+		if (strcmp(op, "push") == 0)
+			pushOp(&stack, line_number, pushNum);
+	}
+	free(buffer), fclose(file), free_stack(&stack);
+	return (EXIT_SUCCESS);
 }
+/**
+ *get_func - get a func of some kind
+ *@stack: the whole stack
+ *@l: the line number
+ *@code: the code being brought in
+ *Return: what ever func is right
+ */
 
+void (*get_func(stack_t **stack, int l, char *code))(stack_t **, unsigned int)
+{
+	instruction_t instruction[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop}
+	};
+	int i = 0;
+
+	while (strcmp(code, instruction[i].opcode) != 0)
+	{
+		i++;
+		if (i > 7)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", l, code);
+			free_stack(stack);
+			error();
+		}
+	}
+	return (instruction[i].f);
+}
+/**
+ *pushOp - helps the push operation
+ *@stack: is a linked list
+ *@line_number: is a counter
+ *@pushNum: adds to a new node
+ */
+void pushOp(stack_t **stack, unsigned int line_number, char *pushNum)
+{
+	if (strcmp(pushNum, "0") == 0)
+		(*stack)->n = 0;
+	if (strcmp(pushNum, "0") != 0)
+	{
+		(*stack)->n = atoi(pushNum);
+		if ((*stack)->n == 0 || (pushNum[0] != '-' && (*stack)->n == -1))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			free_stack(stack);
+			error();
+		}
+	}
+}
+/**
+ *error - error is bad
+ *Return: an exit
+ */
+void error(void)
+{
+	fclose(file);
+	exit(EXIT_FAILURE);
+}
--- monty.h
+++ monty.h
@@ -2,27 +2,56 @@
 #define MONTY_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
-typedef struct stack_node {
-    int value;
-    struct stack_node *next;
-} stack_node_t;
+/* data structures*/
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
 
-stack_node_t *push(stack_node_t *top, int value);
-void pall(stack_node_t *top);
-void pint(stack_node_t *top, int line_number);
-void swap(stack_node_t **top, int line_number);
-void add(stack_node_t **top, int line_number);
-void nop(void);
-void free_stack(stack_node_t *top);
-int is_integer(const char *str);
-void usage_error(void);
-void file_open_error(const char *filename);
-void unknown_instruction_error(int line_number, const char *opcode);
-void malloc_error(void);
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
-#endif /* MONTY_H */
+/*prototypes*/
+void free_stack(stack_t **stack);
+void (*get_func(stack_t **stack, int l, char *code))(stack_t **, unsigned int);
+void error(void);
+void pushOp(stack_t **stack, unsigned int line_number, char *pushNum);
 
+/*opcodes*/
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+/*global variable*/
+extern FILE *file;
+
+#endif
